b4235a6132ffb88f60e495b9df0d35697da7c1ff
Opebiodun <opebiodun@gmail.com>
2024-03-04 19:23:26 +0100

    Capstone Project

--- terraformcode-to-create-jenkins-server/backend.tf
+++ terraformcode-to-create-jenkins-server/backend.tf
@@ -1,7 +1,7 @@
-terraform {
- backend "s3" {
-    bucket = "caps-app"
-   region = "eu-west-2"
-    key = "jenkins-server/terraform.tfstate"
- }
-}
+#terraform {
+#backend "s3" {
+#    bucket = "caps-app"
+#   region = "eu-west-2"
+#    key = "jenkins-server/terraform.tfstate"
+# }
+#}
